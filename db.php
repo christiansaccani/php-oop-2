febfd733e4f5198169e0e91e8da10803c5fd06e6
christiansaccani <christian.saccani99@gmail.com>
2024-04-03 18:26:10 +0200

    ex done

--- db.php
+++ db.php
@@ -5,10 +5,11 @@ require './Models/Item.php';
 // ______________________________________________________
 
 $items = [
-    new Item("Spazzolino per Cani", 9.99, "Cani", "Prodotto"),
-    new Item("Croccantini per Gatti", 14.99, "Gatti", "Cibo"),
-    new Item("Palla da Lancio per Cani", 5.49, "Cani", "Gioco"),
+    new Item("Spazzolino per Cani", 9.99, '<i class="fa-solid fa-dog"></i>', "Prodotto"),
+    new Item("Croccantini per Gatti", 14.99, '<i class="fa-solid fa-cat"></i>', "Cibo"),
+    new Item("Palla da Lancio per Cani", 5.49, '<i class="fa-solid fa-dog"></i>', "Gioco"),
 ];
 
 
 $items[1]->image = "https://arcaplanet.vtexassets.com/arquivos/ids/224803-200-200/virtus-seven-seas-gatto-adult.jpg?v=637454746350170000";
+$items[2]->image = "https://media.istockphoto.com/id/137345149/it/foto/pallina-da-tennis.jpg?s=612x612&w=0&k=20&c=BWH1yhVOL05JIAcjhexiigaTBlv2jqiICERcj7sWq70=";
--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
 
+require './db.php';
+
 ?>
+
 <!DOCTYPE html>
 <html lang="it">
 <head>
@@ -8,14 +11,48 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP - OOP 2</title>
 
+     <!-- fontawesome -->
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
     <!-- bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <style>
+        .card-img-top {
+            width: 100%;
+            height: 300px;
+
+            object-fit: cover;
+        }
+    </style>
 
 </head>
 <body>
     
 <div class="container py-5">
     <h1>PHP - OOP 2</h1>
+
+    <div class="row row-cols-3">
+
+        <?php
+        foreach($items as $item) {
+        ?>
+            <div class="col d-flex justify-content-center">
+                <div class="card" style="width: 18rem;">
+                    <img src="<?= $item->image ?>" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="item-title"><?= $item->title ?></h5>
+                        <h6 class="item-price text-info "><?= $item->price?></h6>
+                        <p class="item-category text-info "><?= $item->category?></p>
+                        <p class="item-type text-info "><?= $item->type?></p>
+                    </div>
+                </div>
+            </div>
+        <?php
+        }
+        ?>
+
+    </div>
 
 </div>
 
